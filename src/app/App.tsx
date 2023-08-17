4e1c98fc9bde157d34a7f966b67ef5a5222d70f8
mspolermo <mspolermo@gmail.com>
2023-08-17 09:59:54 +0500

    changed according to fearure-sliced design

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router";
-import '././styles/index.scss'
+import '././styles/index.scss';
 import { Link } from "react-router-dom";
-import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
-import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense, useContext, useState } from "react";
-import { Theme, ThemeContext } from "./theme/ThemeContext";
-import { useTheme } from "./theme/useTheme";
-import { classNames } from "./helpers/classNames/classNames";
+import { Suspense } from "react";
+
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AboutPage } from "pages/AboutPage";
+import { MainPage } from "pages/MainPage";
 
 const App = () => {
     const {theme, toogleTheme} = useTheme();
@@ -18,8 +18,8 @@ const App = () => {
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
-                    <Route path={'/about'} element={<AboutPageAsync />}/>
-                    <Route path={'/'} element={<MainPageAsync />}/>
+                    <Route path={'/about'} element={<AboutPage/>}/>
+                    <Route path={'/'} element={<MainPage />}/>
                 </Routes>
             </Suspense>    
         </div>
