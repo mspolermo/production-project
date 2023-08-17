c7f3fb88b4ec35d6a659cfc6aed8c289a30342fd
mspolermo <mspolermo@gmail.com>
2023-08-17 11:48:25 +0500

    Router logic created

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
-import { Route, Routes } from "react-router";
+import React from 'react';
 import '././styles/index.scss';
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from './providers/router';
 
 const App = () => {
     const {theme, toogleTheme} = useTheme();
@@ -16,12 +14,7 @@ const App = () => {
             <button onClick={toogleTheme}>TOOGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>    
+            <AppRouter />
         </div>
     )
 }
