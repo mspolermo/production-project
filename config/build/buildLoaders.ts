e7e959eadb174a10d660993c34a8cf0b4a522489
mspolermo <mspolermo@gmail.com>
2023-08-21 17:42:10 +0500

    Webpack hot module replacement and babel (with i18n plugin) added

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -43,9 +43,30 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ['@babel/preset-env'],
+        "plugins": [
+          [
+            "i18next-extract", 
+          {
+            locales: ['ru', 'en'],
+            keyAsDefaultValue: true
+          }
+          ],
+        ]
+      }
+    }
+  }
+
   return [
     fileLoader,
     svgLoader,
+    babelLoader,
     typescriptLoader,
     cssLoader
   ]
