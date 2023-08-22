f324ed0d5c71892bfef890668ddd8ca1cbc2f0dc
mspolermo <mspolermo@gmail.com>
2023-08-22 13:58:24 +0500

    stylelint and i18nplugin for eslint added

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,23 +1,27 @@
 import React, { Suspense } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Route, Routes } from 'react-router-dom';
 import { RouteConfig } from 'shared/config/routeConfig/routeConfig';
 
-const AppRouter = () => (
-    <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-            {Object.values(RouteConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
-                        <div className="page-wrapper">
-                            {element}
-                        </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
-);
+const AppRouter = () => {
+    const { t } = useTranslation();
+    return (
+        <Suspense fallback={<div>{t('Загрузка')}</div>}>
+            <Routes>
+                {Object.values(RouteConfig).map(({ element, path }) => (
+                    <Route
+                        key={path}
+                        path={path}
+                        element={(
+                            <div className="page-wrapper">
+                                {element}
+                            </div>
+                        )}
+                    />
+                ))}
+            </Routes>
+        </Suspense>
+    );
+};
 
 export default AppRouter;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -9,6 +10,7 @@ interface SidebarProps {
 }
 
 export const Sidebar = ({ className }: SidebarProps) => {
+    const { t } = useTranslation();
     const [collapsed, setCollapsed] = useState(false);
 
     const onToggle = () => {
@@ -17,7 +19,7 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
     return (
         <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-            <button type="button" onClick={onToggle}>toggle</button>
+            <button type="button" onClick={onToggle}>{t('Переключить')}</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
