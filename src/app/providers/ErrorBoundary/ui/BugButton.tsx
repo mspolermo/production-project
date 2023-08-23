6089c9314ef3c07475551438e509b87fea827bf1
mspolermo <mspolermo@gmail.com>
2023-08-23 17:12:18 +0500

    ErrorBoundary added

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+
+// Компонент для тестирования
+
+export const BugButton = () => {
+    const { t } = useTranslation();
+    const [error, setError] = useState(false);
+
+    const onThrow = () => {
+        setError(true);
+    };
+
+    useEffect(() => {
+        if (error) {
+            throw new Error();
+        }
+    }, [error]);
+
+    return (
+        <Button onClick={onThrow}>
+            { t('Проброс ошибки') }
+        </Button>
+    );
+};
