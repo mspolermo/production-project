bfc1622dc77be94133ec847e0d591b4c399d5655
mspolermo <mspolermo@gmail.com>
2023-08-24 11:42:10 +0500

    Bundle Analyzer added

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -4,16 +4,6 @@
  */
 
 export default {
-    // All imported modules in your tests should be mocked automatically
-    // automock: false,
-
-    // Stop running tests after `n` failures
-    // bail: 0,
-
-    // The directory where Jest should store its cached dependency information
-    // cacheDirectory: "/private/var/folders/fn/bqj705797h9db56qg0y1qyjc0000gn/T/jest_dx",
-
-    // Automatically clear mock calls, instances and results before every test
     clearMocks: true,
     testEnvironment: 'jsdom',
     coveragePathIgnorePatterns: [
