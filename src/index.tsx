00d56eefb07660623c66c7c3788296b1682d3487
mspolermo <mspolermo@gmail.com>
2023-08-31 22:32:16 +0500

    add redux store, add first entity

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,21 @@ import { render } from 'react-dom';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { ErrorBoundary } from 'app/providers/ErrorBoundary';
+import { StoreProvider } from 'app/providers/StoreProvider';
 import App from './app/App';
 import 'app/styles/index.scss';
-
 import './shared/config/i18n/i18n';
-// eslint-disable-next-line import/order
-import { ErrorBoundary } from 'app/providers/ErrorBoundary';
 
 render(
-    <BrowserRouter>
-        <ErrorBoundary>
-            <ThemeProvider>
-                <App />
-            </ThemeProvider>
-        </ErrorBoundary>
-    </BrowserRouter>,
+    <StoreProvider>
+        <BrowserRouter>
+            <ErrorBoundary>
+                <ThemeProvider>
+                    <App />
+                </ThemeProvider>
+            </ErrorBoundary>
+        </BrowserRouter>
+    </StoreProvider>,
     document.getElementById('root'),
 );
