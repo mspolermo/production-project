113258d4021d2d6777cedc7a4e0a996c15d3bc0c
mspolermo <mspolermo@gmail.com>
2023-09-28 17:31:26 +0500

    add ui state, scroll restore

--- src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
+++ src/features/addCommentForm/model/selectors/addCommentFormSelectors.ts
@@ -1,4 +1,4 @@
 import { StateSchema } from 'app/providers/StoreProvider';
 
-export const getAddCommentFromText = (state: StateSchema) => state.addCommentForm?.text || '';
+export const getAddCommentFromText = (state: StateSchema) => state.addCommentForm?.text ?? '';
 export const getAddCommentFromError = (state: StateSchema) => state.addCommentForm?.error;
