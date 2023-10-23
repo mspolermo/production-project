9fda762de306a6d06f61f78c93b623156c66effa
mspolermo <mspolermo@gmail.com>
2023-10-23 13:05:07 +0500

    add dropdown, update navbar, add avatar with menu

--- src/shared/ui/ListBox/ListBox.stories.tsx
+++ src/shared/ui/ListBox/ListBox.stories.tsx
@@ -8,9 +8,63 @@ export default {
     argTypes: {
         backgroundColor: { control: 'color' },
     },
+    decorators: [
+        (Story) => <div style={{ padding: 150 }}><Story /></div>,
+    ],
 } as ComponentMeta<typeof ListBox>;
 
 const Template: ComponentStory<typeof ListBox> = (args) => <ListBox {...args} />;
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+    value: '123',
+    items: [
+        { content: 'qewdq21e', value: '123' },
+        { content: 'qewdasdq21e', value: '13223' },
+        { content: 'qewasdfdq21e', value: '122343' },
+    ],
+};
+
+export const topLeft = Template.bind({});
+topLeft.args = {
+    direction: 'top left',
+    value: '123',
+    items: [
+        { content: 'qewdq21e', value: '123' },
+        { content: 'qewdasdq21e', value: '13223' },
+        { content: 'qewasdfdq21e', value: '122343' },
+    ],
+};
+
+export const topRight = Template.bind({});
+topRight.args = {
+    direction: 'top right',
+    value: '123',
+    items: [
+        { content: 'qewdq21e', value: '123' },
+        { content: 'qewdasdq21e', value: '13223' },
+        { content: 'qewasdfdq21e', value: '122343' },
+    ],
+};
+
+export const bottomLeft = Template.bind({});
+bottomLeft.args = {
+    direction: 'bottom left',
+    value: '123',
+    items: [
+        { content: 'qewdq21e', value: '123' },
+        { content: 'qewdasdq21e', value: '13223' },
+        { content: 'qewasdfdq21e', value: '122343' },
+    ],
+};
+
+export const bottomRight = Template.bind({});
+bottomRight.args = {
+    direction: 'bottom right',
+    value: '123',
+    items: [
+        { content: 'qewdq21e', value: '123' },
+        { content: 'qewdasdq21e', value: '13223' },
+        { content: 'qewasdfdq21e', value: '122343' },
+    ],
+};
