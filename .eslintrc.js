5ebde4341d6ff4d8c1fb5dd84ba2f37e608687ec
mspolermo <mspolermo@gmail.com>
2023-12-11 19:09:54 +0500

    add test for isolated components with cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,76 +1,76 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    jest: true
-  },
-  extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
+    env: {
+        browser: true,
+        es2021: true,
+        jest: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks', 'mspolermo-plugin', 'unused-imports'],
-  rules: {
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'error',
-    'unused-imports/no-unused-imports': 'error',
-    'i18next/no-literal-string': ['error', {
-      markupOnly: true,
-      ignoreAttribute: ['data-testid', 'to', 'target', 'justify', 'align', 'border', 'gap', 'direction', 'role', 'as']
-    }],
-    'react/jsx-indent': [2, 4],
-    'react/jsx-indent-props': [2, 4],
-    indent: [2, 4],
-    'react/jsx-filename-extension': [2, {
-      extensions: ['.js', '.jsx', '.tsx']
-    }],
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'no-unused-vars': 'warn',
-    'react/require-default-props': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-props-no-spreading': 'warn',
-    'react/function-component-definition': 'off',
-    'no-shadow': 'off',
-    'import/extensions': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off',
-    'max-len': ['error', {
-      ignoreComments: true,
-      code: 130
-    }],
-    'jsx-a11y/no-static-element-interactions': 'off',
-    'jsx-a11y/click-events-have-key-events': 'off',
-    'no-param-reassign': 'off',
-    'no-undef': 'off',
-    'react/no-array-index-key': 'off',
-    'arrow-body-style': 'off',
-    'mspolermo-plugin/path-checker': ['error', {
-      alias: '@'
-    }],
-    'mspolermo-plugin/public-api-imports': ['error', {
-      alias: '@',
-      testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx']
-    }],
-    'mspolermo-plugin/layer-imports': ['error', {
-      alias: '@',
-      ignoreImportPatterns: ['**/StoreProvider', '**/testing']
-    }]
-  },
-  globals: {
-    __IS_DEV__: true,
-    __API__: true,
-    __PROJECT__: true
-  },
-  overrides: [{
-    files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+    extends: ['plugin:react/recommended', 'airbnb', 'plugin:i18next/recommended', 'plugin:storybook/recommended'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'i18next', 'react-hooks', 'mspolermo-plugin', 'unused-imports'],
     rules: {
-      'i18next/no-literal-string': 'off',
-      'max-len': 'off'
-    }
-  }]
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'error',
+        'unused-imports/no-unused-imports': 'error',
+        'i18next/no-literal-string': ['error', {
+            markupOnly: true,
+            ignoreAttribute: ['data-testid', 'to', 'target', 'justify', 'align', 'border', 'gap', 'direction', 'role', 'as'],
+        }],
+        'react/jsx-indent': [2, 4],
+        'react/jsx-indent-props': [2, 4],
+        indent: [2, 4],
+        'react/jsx-filename-extension': [2, {
+            extensions: ['.js', '.jsx', '.tsx'],
+        }],
+        'import/no-unresolved': 'off',
+        'import/prefer-default-export': 'off',
+        'no-unused-vars': 'warn',
+        'react/require-default-props': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-props-no-spreading': 'warn',
+        'react/function-component-definition': 'off',
+        'no-shadow': 'off',
+        'import/extensions': 'off',
+        'import/no-extraneous-dependencies': 'off',
+        'no-underscore-dangle': 'off',
+        'max-len': ['error', {
+            ignoreComments: true,
+            code: 130,
+        }],
+        'jsx-a11y/no-static-element-interactions': 'off',
+        'jsx-a11y/click-events-have-key-events': 'off',
+        'no-param-reassign': 'off',
+        'no-undef': 'off',
+        'react/no-array-index-key': 'off',
+        'arrow-body-style': 'off',
+        // 'mspolermo-plugin/path-checker': ['error', {
+        //     alias: '@',
+        // }],
+        // 'mspolermo-plugin/public-api-imports': ['error', {
+        //     alias: '@',
+        //     testFilesPatterns: ['**/*.test.*', '**/*.story.*', '**/StoreDecorator.tsx'],
+        // }],
+        // 'mspolermo-plugin/layer-imports': ['error', {
+        //     alias: '@',
+        //     ignoreImportPatterns: ['**/StoreProvider', '**/testing'],
+        // }],
+    },
+    globals: {
+        __IS_DEV__: true,
+        __API__: true,
+        __PROJECT__: true,
+    },
+    overrides: [{
+        files: ['**/src/**/*.{test,stories}.{ts,tsx}'],
+        rules: {
+            'i18next/no-literal-string': 'off',
+            'max-len': 'off',
+        },
+    }],
 };
--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -1,27 +1,50 @@
-import { ReducersMapObject } from '@reduxjs/toolkit';
-import { render } from '@testing-library/react';
 import { ReactNode } from 'react';
+import { render } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 import { MemoryRouter } from 'react-router-dom';
-import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import { ReducersMapObject } from '@reduxjs/toolkit';
 import i18nForTests from '@/shared/config/i18n/i18nForTests';
+import { StateSchema, StoreProvider } from '@/app/providers/StoreProvider';
+import { Theme } from '@/shared/const/theme';
+import { ThemeProvider } from '@/app/providers/ThemeProvider';
+import '@/app/styles/index.scss';
 
 export interface componentRenderOptions {
     route?: string;
     initialState?: DeepPartial<StateSchema>;
     asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+    theme?: Theme;
 }
 
-export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
-    const { route = '/', initialState, asyncReducers } = options;
+interface TestProviderProps {
+    children: ReactNode;
+    options?: componentRenderOptions;
+}
 
-    return render(
+export function TestProvider(props: TestProviderProps) {
+    const { children, options = {} } = props;
+    const {
+        route = '/',
+        initialState,
+        asyncReducers,
+        theme = Theme.LIGHT,
+    } = options;
+
+    return (
         <MemoryRouter initialEntries={[route]}>
             <StoreProvider asyncReducers={asyncReducers} initialState={initialState}>
                 <I18nextProvider i18n={i18nForTests}>
-                    {component}
+                    <ThemeProvider initialTheme={theme}>
+                        <div className={`app ${theme}`}>
+                            {children}
+                        </div>
+                    </ThemeProvider>
                 </I18nextProvider>
             </StoreProvider>
-        </MemoryRouter>,
+        </MemoryRouter>
     );
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+    return render(<TestProvider options={options}>{component}</TestProvider>);
 }
