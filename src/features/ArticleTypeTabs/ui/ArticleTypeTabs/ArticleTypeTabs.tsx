9fdfd49eb0ed096442cd1c8b6a0a8ee4513462d8
mspolermo <mspolermo@gmail.com>
2024-02-02 15:10:58 +0500

    ArticlesPage redesigned

--- src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
+++ src/features/ArticleTypeTabs/ui/ArticleTypeTabs/ArticleTypeTabs.tsx
@@ -1,8 +1,10 @@
-import React, { memo, useCallback, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
+import { memo, useCallback, useMemo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { TabItem, Tabs } from '@/shared/ui/depricated/Tabs';
+import { TabItem, Tabs as TabsDeprecated } from '@/shared/ui/depricated/Tabs';
 import { ArticleType } from '@/entities/Article';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { Tabs } from '@/shared/ui/redesigned/Tabs';
 
 interface ArticleTypeTabsProps {
     className?: string;
@@ -12,7 +14,7 @@ interface ArticleTypeTabsProps {
 
 export const ArticleTypeTabs = memo((props: ArticleTypeTabsProps) => {
     const { className, value, onChangeType } = props;
-    const { t } = useTranslation('article');
+    const { t } = useTranslation();
 
     const typeTabs = useMemo<TabItem[]>(
         () => [
@@ -44,11 +46,25 @@ export const ArticleTypeTabs = memo((props: ArticleTypeTabsProps) => {
     );
 
     return (
-        <Tabs
-            className={classNames('', {}, [className])}
-            tabs={typeTabs}
-            value={value}
-            onTabClick={onTabClick}
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+                <Tabs
+                    direction="column"
+                    tabs={typeTabs}
+                    value={value}
+                    onTabClick={onTabClick}
+                    className={classNames('', {}, [className])}
+                />
+            }
+            off={
+                <TabsDeprecated
+                    tabs={typeTabs}
+                    value={value}
+                    onTabClick={onTabClick}
+                    className={classNames('', {}, [className])}
+                />
+            }
         />
     );
 });
