356ed0a9340970beaa71d71924f0f9cfebb57ea3
mspolermo <mspolermo@gmail.com>
2024-02-08 18:00:47 +0500

    start ArticleListItem redesign

--- src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItem.tsx
@@ -1,25 +1,11 @@
-import React, { HTMLAttributeAnchorTarget, memo } from 'react';
-import { useTranslation } from 'react-i18next';
-import { classNames } from '@/shared/lib/classNames/classNames';
-import { Text } from '@/shared/ui/depricated/Text';
-import EyeIcon from '@/shared/assets/icons/eye-20-20.svg';
-import { Icon } from '@/shared/ui/depricated/Icon';
-import { Card } from '@/shared/ui/depricated/Card';
-import { Avatar } from '@/shared/ui/depricated/Avatar';
-import { Button, ButtonTheme } from '@/shared/ui/depricated/Button';
-import { getRouteArticleDetails } from '@/shared/const/router';
-import { AppLink } from '@/shared/ui/depricated/AppLink';
-import {
-    ArticleBlockType,
-    ArticleView,
-} from '../../model/consts/articleConsts';
-import { Article, ArticleTextBlock } from '../../model/types/article';
-import cls from './ArticleListItem.module.scss';
-import { ArticleTextBlockComponent } from '../ArticleTextBlockComponent/ArticleTextBlockComponent';
-import { AppImage } from '@/shared/ui/redesigned/AppImage';
-import { Skeleton } from '@/shared/ui/depricated/Skeleton';
+import { HTMLAttributeAnchorTarget, memo } from 'react';
+import { ArticleView } from '../../model/consts/articleConsts';
+import { Article } from '../../model/types/article';
+import { ToggleFeatures } from '@/shared/lib/features';
+import { ArticleListItemDeprecated } from './ArticleListItemDeprecated/ArticleListItemDeprecated';
+import { ArticleListItemRedesigned } from './ArticleListItemRedesigned/ArticleListItemRedesigned';
 
-interface ArticleListItemProps {
+export interface ArticleListItemProps {
     className?: string;
     article: Article;
     view: ArticleView;
@@ -27,95 +13,11 @@ interface ArticleListItemProps {
 }
 
 export const ArticleListItem = memo((props: ArticleListItemProps) => {
-    const { className, article, view, target } = props;
-    const { t } = useTranslation('article');
-
-    const types = <Text text={article.type.join(', ')} className={cls.types} />;
-    const views = (
-        <>
-            <Text text={String(article.views)} className={cls.views} />
-            <Icon Svg={EyeIcon} />
-        </>
-    );
-
-    if (view === ArticleView.BIG) {
-        const textBlock = article.blocks.find(
-            (block) => block.type === ArticleBlockType.TEXT,
-        ) as ArticleTextBlock;
-
-        return (
-            <div
-                data-testid="ArticleListItem"
-                className={classNames(cls.ArticleListItem, {}, [
-                    className,
-                    cls[view],
-                ])}
-            >
-                <Card className={cls.card}>
-                    <div className={cls.header}>
-                        <Avatar size={30} src={article.user.avatar} />
-                        <Text
-                            text={article.user.username}
-                            className={cls.username}
-                        />
-                        <Text text={article.createdAt} className={cls.date} />
-                    </div>
-                    <Text text={article.title} className={cls.title} />
-                    {types}
-                    <AppImage
-                        fallback={<Skeleton width="100%" height={250} />}
-                        alt={article.title}
-                        src={article.img}
-                        className={cls.img}
-                    />
-                    {textBlock && (
-                        <ArticleTextBlockComponent
-                            block={textBlock}
-                            className={cls.textBlock}
-                        />
-                    )}
-                    <div className={cls.footer}>
-                        <AppLink
-                            to={getRouteArticleDetails(article.id)}
-                            target={target}
-                        >
-                            <Button theme={ButtonTheme.OUTLINE}>
-                                {t('Читать далее')}
-                            </Button>
-                        </AppLink>
-                        {views}
-                    </div>
-                </Card>
-            </div>
-        );
-    }
-
     return (
-        <AppLink
-            data-testid="ArticleListItem"
-            to={getRouteArticleDetails(article.id)}
-            target={target}
-            className={classNames(cls.ArticleListItem, {}, [
-                className,
-                cls[view],
-            ])}
-        >
-            <Card className={cls.card}>
-                <div className={cls.imageWrapper}>
-                    <AppImage
-                        fallback={<Skeleton width={200} height={200} />}
-                        alt={article.title}
-                        src={article.img}
-                        className={cls.img}
-                    />
-                    <Text text={article.createdAt} className={cls.date} />
-                </div>
-                <div className={cls.infoWrapper}>
-                    {types}
-                    {views}
-                </div>
-                <Text text={article.title} className={cls.title} />
-            </Card>
-        </AppLink>
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={<ArticleListItemRedesigned {...props} />}
+            off={<ArticleListItemDeprecated {...props} />}
+        />
     );
 });
--- src/entities/Article/ui/ArticleListItem/ArticleListItemSkeleton.tsx
+++ src/entities/Article/ui/ArticleListItem/ArticleListItemSkeleton.tsx
@@ -1,9 +1,12 @@
 import { memo } from 'react';
 import { classNames } from '@/shared/lib/classNames/classNames';
-import { Card } from '@/shared/ui/depricated/Card';
-import { Skeleton } from '@/shared/ui/depricated/Skeleton';
+import { Card as CardDeprecated } from '@/shared/ui/depricated/Card';
+import { Card as CardRedesigned } from '@/shared/ui/redesigned/Card';
+import { Skeleton as SkeletonDeprecated } from '@/shared/ui/depricated/Skeleton';
+import { Skeleton as SkeletonRedesigned } from '@/shared/ui/redesigned/Skeleton';
 import { ArticleView } from '../../model/consts/articleConsts';
 import cls from './ArticleListItem.module.scss';
+import { toggleFeatures } from '@/shared/lib/features';
 
 interface ArticleListItemSkeletonProps {
     className?: string;
@@ -13,6 +16,17 @@ interface ArticleListItemSkeletonProps {
 export const ArticleListItemSkeleton = memo(
     (props: ArticleListItemSkeletonProps) => {
         const { className, view } = props;
+
+        const Skeleton = toggleFeatures({
+            name: 'isAppRedesigned',
+            on: () => SkeletonRedesigned,
+            off: () => SkeletonDeprecated,
+        });
+        const Card = toggleFeatures({
+            name: 'isAppRedesigned',
+            on: () => CardRedesigned,
+            off: () => CardDeprecated,
+        });
 
         if (view === ArticleView.BIG) {
             return (
@@ -24,7 +38,7 @@ export const ArticleListItemSkeleton = memo(
                 >
                     <Card className={cls.card}>
                         <div className={cls.header}>
-                            <Skeleton width={30} height={30} border="100%" />
+                            <Skeleton border="50%" height={30} width={30} />
                             <Skeleton
                                 width={150}
                                 height={16}
@@ -43,7 +57,7 @@ export const ArticleListItemSkeleton = memo(
                         />
                         <Skeleton height={200} className={cls.img} />
                         <div className={cls.footer}>
-                            <Skeleton width={200} height={36} />
+                            <Skeleton height={36} width={200} />
                         </div>
                     </Card>
                 </div>
@@ -60,15 +74,15 @@ export const ArticleListItemSkeleton = memo(
                 <Card className={cls.card}>
                     <div className={cls.imageWrapper}>
                         <Skeleton
-                            height={200}
                             width={200}
+                            height={200}
                             className={cls.img}
                         />
                     </div>
                     <div className={cls.infoWrapper}>
-                        <Skeleton height={16} width={130} />
+                        <Skeleton width={130} height={16} />
                     </div>
-                    <Skeleton height={16} width={150} className={cls.title} />
+                    <Skeleton width={150} height={16} className={cls.title} />
                 </Card>
             </div>
         );
