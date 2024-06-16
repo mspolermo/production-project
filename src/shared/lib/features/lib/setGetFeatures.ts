b0623903688d943513807f6c909cdcb5de584525
mspolermo <mspolermo@gmail.com>
2024-06-16 18:37:19 +0500

    default Design changed to reDesign

--- src/shared/lib/features/lib/setGetFeatures.ts
+++ src/shared/lib/features/lib/setGetFeatures.ts
@@ -1,12 +1,11 @@
-import { LOCAL_STORAGE_LAST_DESIGN_KEY} from '@/shared/const/localstorage';
 import { FeatureFlags } from '@/shared/types/featureFlags';
 
 const defaultFeatures: FeatureFlags = {
-    isAppRedesigned: localStorage.getItem(LOCAL_STORAGE_LAST_DESIGN_KEY) === 'new'
+    isAppRedesigned: true,
 };
 // ФИЧИ НЕ МЕНЯЮТСЯ В ХОДЕ СЕССИИ, ИХ НЕОБЯЗАТЕЛЬНО ДЕЛАТЬ РЕАКТИВНЫМИ!
 let featureFlags: FeatureFlags = {
-    ...defaultFeatures
+    ...defaultFeatures,
 };
 
 export function setFeatureFlags(newFeatureFlags?: FeatureFlags) {
